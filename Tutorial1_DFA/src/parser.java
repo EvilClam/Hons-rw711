772aa40c6da1476f5505c2b87da778320bd545ec
EvilClam <16715128@sun.ac.za>
2014-02-06 16:35:36 +0200

    Done with automaton, states, alphabet and tFunction.

--- Tutorial1_DFA/src/parser.java
+++ Tutorial1_DFA/src/parser.java
@@ -1,20 +1,64 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 
 public class parser {
 	scanner sc = new scanner();
+	TOKEN currentToken;
+	ArrayList<String> states;
 	public static void main (String [] args) {
+		parser s= new parser();
+		try {
+			s.setup("test1");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+	}
+	public void setup(String dfa_file) throws FileNotFoundException {
+		sc.setup(dfa_file);
+		automaton();
 	}
 	
 	public void automaton() {
-		
+		expectToken(TOKEN.Types.DFA);
+		expectToken(TOKEN.Types.EQUALS);
+		expectToken(TOKEN.Types.OPEN_PARENTHESIS);
+		states();
+		expectToken(TOKEN.Types.COMMA);
+		alphabet();
+		expectToken(TOKEN.Types.COMMA);
+		tfunction();
+		expectToken(TOKEN.Types.COMMA);
+		start();
+		expectToken(TOKEN.Types.COMMA);
+		accept();
+		expectToken(TOKEN.Types.CLOSED_PARENTHESIS);
 	}
 	
 	public void states() {
-		
+		states = idset();
 	}
 	
 	public void alphabet() {
-		
+		expectToken(TOKEN.Types.OPEN_BRACE);
+		expectToken(TOKEN.Types.SYMBOL);
+		while (TOKEN.Types.CLOSED_BRACE != currentToken.getType()) {
+			expectToken(TOKEN.Types.COMMA);
+			expectToken(TOKEN.Types.SYMBOL);
+		}
+		expectToken(TOKEN.Types.CLOSED_BRACE);
+	}
+	
+	public void tfunction() {
+		expectToken(TOKEN.Types.OPEN_BRACE);
+		expectToken(TOKEN.Types.SYMBOL);
+		while (TOKEN.Types.CLOSED_BRACE != currentToken.getType()) {
+			expectToken(TOKEN.Types.COMMA);
+			expectToken(TOKEN.Types.SYMBOL);
+		}
+		expectToken(TOKEN.Types.CLOSED_BRACE);
 	}
 	
 	public void start() {
@@ -25,7 +69,25 @@ public class parser {
 		
 	}
 	
-	public boolean expect(TOKEN.Types type) {
+	public ArrayList<String> idset() {
+		return null;
+	}
+	
+	public String[] map() {
+		return new String[3];
+	}
+	
+	
+	
+	public boolean expectToken(TOKEN.Types type) {
 		return type == sc.next().getType();
+	}
+}
+
+class IllegalDFAFormatException extends Exception
+{
+	public String toString() 
+	{
+		return "IllegalDFAFormatException";
 	}
 }
