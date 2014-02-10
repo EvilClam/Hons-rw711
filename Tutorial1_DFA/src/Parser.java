a4b4fd33af2f3771ca18a690910655cf88e33dee
EvilClam <16715128@sun.ac.za>
2014-02-10 11:52:50 +0200

    test

--- Tutorial1_DFA/src/Parser.java
+++ Tutorial1_DFA/src/Parser.java
@@ -211,13 +211,9 @@ public class Parser {
 	private String expectID() throws IllegalDFAFormatException, IOException, IllegalTokenException 
 	{
 		String id;
-		if(Token.Types.SYMBOL != currentToken.getType() && Token.Types.ID != currentToken.getType()) {
+		if(Token.Types.ID != currentToken.getType()) {
 			System.out.println("Expected: " + Token.Types.ID + " But found: " +  currentToken.getType());
 			throw new IllegalDFAFormatException();
-		} else if (Token.Types.SYMBOL == currentToken.getType() && currentToken.getLexeme().equals("")) {
-			System.out.println("Expected: " + Token.Types.ID + " But found: " +  currentToken.getType());
-                	throw new IllegalDFAFormatException();
-
 		}
 		id = currentToken.getLexeme();
 		currentToken = sc.next();
